c06885953eb33372e9fbd5efa68f36a241dcdd5d
MekImEne <fi_mekideche@esi.dz>
2022-12-05 16:44:09 +0100

    mock the fetch method

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,17 @@
 import { render, screen } from "@testing-library/react";
+import mockFetch from "./mocks/mockFetch";
 import App from "./App";
+
+beforeEach(() => {
+  //creates a mock function that will track calls to the fetch method 
+  //attached to the global window variable in the DOM.
+  jest.spyOn(window, "fetch").mockImplementation(mockFetch); //use func mockFetch to implement the mock method
+})
+
+afterEach(() => {
+  jest.restoreAllMocks()
+});
+
 
 test("initial render in the landing page", () => {
   render(<App />);
